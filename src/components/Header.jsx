fc909ea1d4e69edb4bba7ec94b161bc84c857444
Jordan <jordancdiaz04@gmail.com>
2023-09-24 03:07:53 -0400

    add flashcard subject description

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import BlackStrat from "../assets/blackstrat.png";
+import LesPaul from "../assets/lespaul.png";
+
+export default function Header() {
+  return (
+    <>
+      <div className="header-wrapper">
+        <img src={BlackStrat} id="strat" />
+        <div className="header">
+          <h1>
+            The <span>Ultimate</span> Guitar Test
+          </h1>
+          <h2>
+            How much of a guitar enthusiast are you? Test your knowledge of
+            guitar and music history
+          </h2>
+          <h3>Number of cards: 11</h3>
+        </div>
+        <img src={LesPaul} id="les-paul" />
+      </div>
+    </>
+  );
+}
